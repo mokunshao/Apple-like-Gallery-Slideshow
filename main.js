50a1415909e36319b9fd7ee7eb7e4b873af9eee8
mokunshao <1627510957@qq.com>
2018-11-04 02:12:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-$('.menus>ul>li').eq(0).addClass('active').siblings().removeClass('active')
+window.onload = function(){
 
-let n = 1
+    $('.menus>ul>li').eq(0).addClass('active').siblings().removeClass('active')
 
-let menusLength = $('.menus>ul>li').length
-
-let autoPlay = setInterval(function(){
-    $('.menus>ul>li').eq(n%menusLength).trigger('click')
-    n++
-},5000)
-
-$('.menus>ul>li').on('click',function(e){
-    let current = e.currentTarget
-    let index = $(current).index()
-    $('.menus>ul>li').eq(index).addClass('active').siblings().removeClass('active')
-    $('.pictures>img').css({transform:`translateX(${-920*index}px)`})
-    n = index
-})
-
-$('.pictures>img').on('mouseenter',function(){
-    window.clearInterval(autoPlay)
-})
-
-$('.pictures>img').on('mouseleave',function(){
-    autoPlay = setInterval(function(){
+    let n = 1
+    
+    let menusLength = $('.menus>ul>li').length
+    
+    let autoPlay = setInterval(function(){
         $('.menus>ul>li').eq(n%menusLength).trigger('click')
         n++
     },5000)
-})
-
-$(document).on('visibilitychange',function(){
-    if(document.hidden){
+    
+    $('.menus>ul>li').on('click',function(e){
+        let current = e.currentTarget
+        let index = $(current).index()
+        $('.menus>ul>li').eq(index).addClass('active').siblings().removeClass('active')
+        $('.pictures>img').css({transform:`translateX(${-920*index}px)`})
+        n = index
+    })
+    
+    $('.pictures>img').on('mouseenter',function(){
         window.clearInterval(autoPlay)
-    }else if(!document.hidden){
+    })
+    
+    $('.pictures>img').on('mouseleave',function(){
         autoPlay = setInterval(function(){
             $('.menus>ul>li').eq(n%menusLength).trigger('click')
             n++
-        },5000)    
-    }
-})
+        },5000)
+    })
+    
+    $(document).on('visibilitychange',function(){
+        if(document.hidden){
+            window.clearInterval(autoPlay)
+        }else if(!document.hidden){
+            autoPlay = setInterval(function(){
+                $('.menus>ul>li').eq(n%menusLength).trigger('click')
+                n++
+            },5000)    
+        }
+    })
+
+}
